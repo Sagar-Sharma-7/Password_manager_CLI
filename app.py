388fd7dc56c41f9faaa6b607b520a08bbfffb314
Sagar-Sharma-7 <6969sagarsharma@gmail.com>
2022-11-03 13:47:51 +0530

    view and add function

--- app.py
+++ app.py
@@ -2,15 +2,26 @@ master_pwd = input("What is the master password?: ")
 
 
 def view():
-    pass
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("Username: ", user, "| Password: ", passw)
+
+def add():
+    name = input("Account name: ")
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + pwd + "\n")
 
 # menu
 while True:
     mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit: ")
     if mode.lower() == "view":
-        pass
+        view()
     elif mode.lower() == "add":
-        pass
+        add()
     elif mode.lower() == "q":
         break
     else: 
